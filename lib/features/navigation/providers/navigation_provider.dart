9836cf080ed38d9e552b5ae63e58e42acf53a495
Emirhan Sen <emirhansen35@gmail.com>
2025-12-16 11:14:46 +0300

    riverpod included

---
+++ lib/features/navigation/providers/navigation_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// 1. Notifier Sınıfı: Bu sınıf "Logic" yani mantığı tutar.
+class NavigationNotifier extends StateNotifier<int> {
+  // Başlangıç değeri 0 (yani Home sekmesi)
+  NavigationNotifier() : super(0);
+
+  // İndeksi değiştiren metod
+  void setIndex(int index) {
+    state = index;
+  }
+
+  // Özel yönlendirme metodları
+  void goToHome() => state = 0;
+  void goToRoute() => state = 1;
+  void goToHistory() => state = 2;
+  void goToProfile() => state = 3;
+}
+
+// 2. Provider Tanımı: Bu değişken "Erişim Noktası"dır.
+// Uygulamanın herhangi bir yerinden bu değişkene ulaşacağız.
+final navigationProvider = StateNotifierProvider<NavigationNotifier, int>((ref) {
+  return NavigationNotifier();
+});
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:deli4route/features/onboarding/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
-  runApp(const MyApp());
+  
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Deli4Route",
@@ -23,4 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
