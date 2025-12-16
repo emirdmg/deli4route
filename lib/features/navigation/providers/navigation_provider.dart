ceb38a36f673b9456f834f54aaa6e7b15c6f1422
Emirhan Sen <emirhansen35@gmail.com>
2025-12-16 17:49:49 +0300

    firebase auth

--- lib/features/navigation/providers/navigation_provider.dart
+++ lib/features/navigation/providers/navigation_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 // 1. Notifier Sınıfı: Bu sınıf "Logic" yani mantığı tutar.
 class NavigationNotifier extends StateNotifier<int> {
