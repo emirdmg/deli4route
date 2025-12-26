c0c09116dc5ca9cb8703e89f4c4e59623ba7682e
Emirhan Sen <emirhansen35@gmail.com>
2025-12-26 11:54:02 +0300

    openstreetmap added

--- lib/features/navigation/pages/route_page.dart
+++ lib/features/navigation/pages/route_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class RoutePage extends StatefulWidget {
   const RoutePage({super.key});
@@ -9,10 +12,31 @@ class RoutePage extends StatefulWidget {
 
 class _RoutePageState extends State<RoutePage> {
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Route'),
+    return Scaffold(
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(41.015137, 28.979530),
+          initialZoom: 13,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            userAgentPackageName: 'com.example.deli4route',
+          ),
+        ],
       ),
     );
   }
